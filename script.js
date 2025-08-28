8673d8d14e8d6b0680ed6c32323ce3d87f6c8955
Juliana <juliana.pontes.almeida@escola.pr.gov.br>
2025-08-28 11:43:53 -0300

    ihyiu

--- script.js
+++ script.js
@@ -3,3 +3,39 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Você costuma usar várias vezes inteligência artificial?"
+        alternativas: Sim  Não 
+    },
+    {
+        enunciado: "A IA pode ser usada para otimizar processos industriais?"
+        alternativas: [
+            sim não 
+        ],
+    },
+    {
+        enunciado: 
+        "Existem preocupações éticas sobre o uso de IA?"
+        alternativas: [
+            sim não 
+        ],
+    };
+    {
+        enunciado: 
+        "A IA pode aprender com falhas para melhorar sua performance?"
+        alternativas: [
+            sim não
+        ],
+    },
+    {
+        enunciado:
+        "voce costuma usar muitas vezes IA?"
+        alternativas: [
+            sim não
+             ],
+    },
+    {
+
+
